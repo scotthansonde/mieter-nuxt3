0e876b44c97785518776bf7bfe96ae25c675d8b2
Scott Hanson <papascott@gmail.com>
2022-10-20 13:05:01 -0500

    Vuetify plugin configured

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,29 @@
+import vuetify from 'vite-plugin-vuetify'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ['@mdi/font/css/materialdesignicons.min.css', 'vuetify/lib/styles/main.sass'],
+  css: [
+    // '@mdi/font/css/materialdesignicons.min.css',
+    'vuetify/styles',
+  ],
   build: {
     transpile: ['vuetify'],
   },
-  modules: ['@pinia/nuxt'],
   vite: {
     define: {
       'process.env.DEBUG': false,
     },
+    ssr: {
+      noExternal: ['vuetify'], // add the vuetify vite plugin
+    },
   },
+  modules: [
+    // @ts-ignore
+    // this adds the vuetify vite plugin
+    // also produces type errors in the current beta release
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => config.plugins.push(vuetify({ autoImport: true })))
+    },
+    '@pinia/nuxt',
+  ],
 })
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,13 +1,17 @@
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { mdi } from 'vuetify/lib/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.min.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
-    components,
-    directives,
     theme: {
       defaultTheme: 'dark',
+    },
+    icons: {
+      defaultSet: 'mdi',
+      sets: {
+        mdi,
+      },
     },
   })
 
