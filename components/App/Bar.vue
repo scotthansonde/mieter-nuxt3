98b95461313e94a8f3bb6b7c12335c75a3366cf8
Scott Hanson <papascott@gmail.com>
2022-11-11 16:22:25 +0100

    Username and logout in Bar.vue

--- components/App/Bar.vue
+++ components/App/Bar.vue
@@ -4,11 +4,25 @@
       Development
       <span v-if="data?.getDBHost"> (db:{{ data.getDBHost.dbHost }}) </span>
     </v-toolbar-title>
+    <v-spacer />
+    <div v-if="AuthStore.user?.name">
+      {{ AuthStore.user?.name }}
+      <v-btn color="red darken-1" text @click="onLogout">Logout</v-btn>
+    </div>
   </v-app-bar>
 </template>
 
 <script setup>
+import { useAuthStore } from '@/stores/AuthStore'
+const AuthStore = useAuthStore()
 const { data } = await useAsyncGql('getDBHost')
+
+const onLogout = () => {
+  const token = useCookie('gql:default')
+  token.value = null
+  AuthStore.setUser(null)
+  return navigateTo('/login')
+}
 </script>
 
 <style lang="scss" scoped></style>
