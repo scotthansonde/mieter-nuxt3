1c5c330a3d5b43aea73445499055c3a29c84eaa4
Scott Hanson <papascott@gmail.com>
2022-11-13 13:53:53 +0100

    /people/[id] page

---
+++ pages/people/[id].vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <nuxt-link to="/people">←People</nuxt-link>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card v-if="data?.getPersonById">
+          <v-card-title>{{ data?.getPersonById.vollername }} </v-card-title>
+          <v-card-subtitle>
+            <div>
+              Personalnummer: {{ data?.getPersonById.personalnummer }} (MyPeople: {{ data?.getPersonById.myPeopleID }})
+            </div>
+            <div>
+              Store:
+              {{ data?.getPersonById.kostenstelle }}
+            </div>
+            <div>Position: {{ data?.getPersonById.position }}</div>
+            <div>
+              {{ `${data?.getPersonById.strasse}, ${data?.getPersonById.plz} ${data?.getPersonById.ort} ` }}
+            </div>
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+const route = useRoute()
+const id = route.params.id
+const { data } = await useAsyncGql('getPersonById', { _id: id })
+</script>
+
+<style lang="scss" scoped></style>
--- pages/people/index.vue
+++ pages/people/index.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>Test</div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h2>People</h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col sm="8" offset-sm="2">
+        <v-text-field v-model="filter" label="Filter Namen oder Personalnummer" placeholder="Tipp zu filtern…" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="p in filteredPeople" :key="p._id" cols="12" sm="6" md="3">
+        <v-card nuxt :to="'/people/' + p._id">
+          <v-card-title>{{ `${p.vorname} ${p.nachname}` }}</v-card-title>
+          <v-card-subtitle>
+            PN: {{ p.personalnummer }} ({{ p.myPeopleID }}) {{ p.myPeopleOrtID }} {{ p.kostenstelle }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
 const { data } = await useAsyncGql('getPeople')
-console.log(data.value.getPeople)
+const filter = ref('')
+const filteredPeople = computed(() => {
+  return data.value.getPeople.filter(
+    (person) =>
+      person.vollername.toLowerCase().includes(filter.value.toLowerCase()) ||
+      person.personalnummer.includes(filter.value)
+  )
+})
 </script>
 
 <style lang="scss" scoped></style>
