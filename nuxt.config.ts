6350f632cea95df061f50f41db1679d280549b87
Scott Hanson <papascott@gmail.com>
2023-05-22 18:48:26 +0200

    Upgrade all packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,22 @@
-import { defineNuxtConfig } from 'nuxt/config'
-import vuetify from 'vite-plugin-vuetify'
+import { defineNuxtConfig } from "nuxt/config";
+import vuetify from "vite-plugin-vuetify";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: [
     // '@mdi/font/css/materialdesignicons.min.css',
-    'vuetify/styles',
+    "vuetify/styles",
   ],
 
   build: {
-    transpile: ['vuetify'],
+    transpile: ["vuetify"],
   },
   vite: {
     define: {
-      'process.env.DEBUG': false,
+      "process.env.DEBUG": false,
     },
     ssr: {
-      noExternal: ['vuetify'], // add the vuetify vite plugin
+      noExternal: ["vuetify"], // add the vuetify vite plugin
     },
   },
   modules: [
@@ -24,17 +24,20 @@ export default defineNuxtConfig({
     // this adds the vuetify vite plugin
     // also produces type errors in the current beta release
     async (options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => config.plugins.push(vuetify({ autoImport: true })))
+      nuxt.hooks.hook("vite:extendConfig", (config) =>
+        config.plugins.push(vuetify({ autoImport: true }))
+      );
     },
-    '@pinia/nuxt',
-    'nuxt-graphql-client',
-    '@nuxtjs/google-fonts',
+    "@pinia/nuxt",
+    "nuxt-graphql-client",
+    "@nuxtjs/google-fonts",
   ],
 
   runtimeConfig: {
     public: {
-      GQL_HOST: 'http://localhost:4000', // overwritten by process.env.GQL_HOST
-      clientId: '812430523790-1t21ltft00a02s792om477spsvrcckmi.apps.googleusercontent.com',
+      GQL_HOST: "http://localhost:4000", // overwritten by process.env.GQL_HOST
+      clientId:
+        "812430523790-1t21ltft00a02s792om477spsvrcckmi.apps.googleusercontent.com",
     },
   },
 
@@ -42,11 +45,11 @@ export default defineNuxtConfig({
     families: {
       Roboto: [100, 300, 400, 500, 700, 900],
     },
-    display: 'swap',
+    display: "swap",
     prefetch: false,
     preconnect: false,
     preload: false,
     download: false,
     base64: false,
   },
-})
+});
--- plugins/gauth.js
+++ plugins/gauth.js
@@ -1,8 +1,10 @@
-import vue3GoogleLogin from 'vue3-google-login'
+import vue3GoogleLogin from "vue3-google-login";
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
   nuxtApp.vueApp.use(vue3GoogleLogin, {
-    clientId: nuxtApp.$config.clientId,
-    popupType: 'TOKEN',
-  })
-})
+    // clientId: nuxtApp.$config.clientId,
+    clientId: config.public.clientId,
+    popupType: "TOKEN",
+  });
+});
