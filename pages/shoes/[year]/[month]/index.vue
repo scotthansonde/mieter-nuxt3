602a971f04615f518c11ef1108f3fa4b28379029
Scott Hanson <papascott@gmail.com>
2023-06-04 09:42:47 +0200

    WIP Redirect to current year/month

---
+++ pages/shoes/[year]/[month]/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Test {{ year }} {{ month }}</div>
+  <pre>{{ data }}</pre>
+</template>
+
+<script setup>
+definePageMeta({ middleware: 'auth' })
+const route = useRoute()
+const { year, month } = route.params
+const { data } = await useFetch(`/api/purchases/${year}/${month}/SCHU`)
+</script>
+
+<style lang="scss" scoped></style>
--- pages/shoes/index.vue
+++ pages/shoes/index.vue
@@ -1,7 +1,11 @@
-<template>
-  <div>Test</div>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
+<script setup>
+definePageMeta({
+  middleware: [
+    'auth',
+    defineNuxtRouteMiddleware((_to, _from) => {
+      const now = new Date()
+      return navigateTo(`/shoes/${now.getFullYear()}/${now.getMonth() + 1}`)
+    }),
+  ],
+})
+</script>
