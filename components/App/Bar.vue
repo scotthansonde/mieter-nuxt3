0e596d2c0cc3eac2f324206f8d627534279551dd
Scott Hanson <papascott@gmail.com>
2023-07-17 15:42:40 +0200

    Only show development in bar to me

--- components/App/Bar.vue
+++ components/App/Bar.vue
@@ -3,8 +3,8 @@
     <v-app-bar-nav-icon v-if="AuthStore.user" @click.stop="NavStore.toggleDrawer" />
     <v-app-bar-nav-icon v-else disabled />
     <v-toolbar-title>
-      Development
-      <span> {{ db?.dbHost }} </span>
+      <span v-if="!isProduction">Development - </span>
+      <span v-if="AuthStore.user?.name === 'Scott Hanson' && status"> {{ db?.dbHost }} </span>
     </v-toolbar-title>
     <v-spacer />
     <div v-if="AuthStore.user?.name && status">
@@ -22,6 +22,7 @@ import { useNavStore } from '@/stores/NavStore'
 const AuthStore = useAuthStore()
 const NavStore = useNavStore()
 const { data: db } = await useFetch(`/api/dbHost`)
+const isProduction = process.env.NODE_ENV === 'production'
 </script>
 
 <style lang="scss" scoped></style>
