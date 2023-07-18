4b223e5ff451ddbd51851372df6bb4095ccbd285
Scott Hanson <papascott@gmail.com>
2023-07-18 15:49:03 +0200

    Remove console logs

--- server/api/webcockpit/index.get.js
+++ server/api/webcockpit/index.get.js
@@ -17,7 +17,6 @@ export default defineEventHandler(async (event) => {
   const startDate = queryParams.startDate
   const endDate = queryParams.endDate
   const login = await loginWebcockpit()
-  console.log(login)
   const response = await fetch('https://www.webcockpit.app/api/Payroll/getPayrollTransactionsData', {
     headers: {
       accept: 'application/json, text/plain, */*',
@@ -27,8 +26,6 @@ export default defineEventHandler(async (event) => {
     body: `{"Period":{"start":"${startDate}","end":"${endDate}"},"Restaurants":[780,484,1400,779,483],"IncludeExported":true,"IncludeNotExported":true,"IncludeRdsEntries":false,"IncludeReflexisEntries":true,"ShowEarnCodesOnly":false,"HidePreviousMonths":true,"IncludeEmployeeMealsRds":true,"IncludeMeinDienstplanEntries":false,"EnableFactorGrouping":false,"SortType":2}`,
     method: 'POST',
   })
-  console.log(response)
   const data = await response.json()
-
   return data
 })
