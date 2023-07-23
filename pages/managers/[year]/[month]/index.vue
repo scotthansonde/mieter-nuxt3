4e5db98632f489b6d760c2730ee8913ccac17a6f
Scott Hanson <papascott@gmail.com>
2023-07-23 15:50:50 +0200

    Link Manager to oldapp

--- pages/managers/[year]/[month]/index.vue
+++ pages/managers/[year]/[month]/index.vue
@@ -3,6 +3,9 @@
     <v-row>
       <v-col>
         <h2>Managers {{ $route.params.year }} - {{ $route.params.month }}</h2>
+        <ul>
+          <li><NuxtLink to="https://oldapp.mcd-nordheide.de/managers">Die Manager App ist noch hier.</NuxtLink></li>
+        </ul>
       </v-col>
     </v-row>
   </v-container>
