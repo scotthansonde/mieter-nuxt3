6ebb00fc7f59eed70d779d6471bf4928b28babf4
Scott Hanson <papascott@gmail.com>
2023-07-25 18:15:05 +0200

    WIP Output list of salary lines in ul

--- pages/managers/[year]/[month]/index.vue
+++ pages/managers/[year]/[month]/index.vue
@@ -6,11 +6,23 @@
         <ul>
           <li><NuxtLink to="https://oldapp.mcd-nordheide.de/managers">Die Manager App ist noch hier.</NuxtLink></li>
         </ul>
+        <ul>
+          <li v-for="m in managers" key="m.managerID">
+            {{ m.store }} {{ m.person.personalnummer }} {{ m.name }} {{ m.position }} {{ m.vertrag }}
+            {{ m.tarifgruppe }} tariflohn zuschlag {{ m.gehalt }} {{ m.fahrtkosten || '0,00€' }} {{ m.bonus }} "{{
+              m.outputtedNote
+            }}"
+          </li>
+        </ul>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<script setup></script>
+<script setup>
+const route = useRoute()
+const { year, month } = route.params
+const { data: managers, refresh } = await useFetch(`/api/managers/salaryLines/${year}/${month}`)
+</script>
 
 <style lang="scss" scoped></style>
--- server/utils/payrollObject.js
+++ server/utils/payrollObject.js
@@ -58,4 +58,59 @@ export const payroll = [
     stores: ['DIB', 'WIN', 'RAD', 'PHX'],
     verwaltung: { FM: 30000 },
   },
+  {
+    payrollMonth: '2021-09',
+    stores: ['DIB', 'WIN', 'RAD', 'PHX'],
+    verwaltung: { FM: 30000 },
+  },
+  {
+    payrollMonth: '2021-10',
+    stores: ['RAD', 'PHX'],
+    verwaltung: { FM: 10000, OM: 111000, BL: 221900 },
+  },
+  {
+    payrollMonth: '2021-11',
+    stores: ['RAD'],
+    verwaltung: { FM: 10000 },
+  },
+  {
+    payrollMonth: '2021-12',
+    stores: ['RAD'],
+    verwaltung: { FM: 10000 },
+  },
+  {
+    payrollMonth: '2022-01',
+    stores: ['RAD', 'PHX'],
+    verwaltung: { FM: 10000 },
+  },
+  {
+    payrollMonth: '2023-02',
+    stores: ['DIB', 'WIN', 'PHX'],
+    verwaltung: { FM: 30000 },
+  },
+  {
+    payrollMonth: '2023-03',
+    stores: ['DIB', 'RAD', 'PHX'],
+    verwaltung: { FM: 20000 },
+  },
+  {
+    payrollMonth: '2023-04',
+    stores: ['DIB', 'RAD', 'WIN', 'PHX'],
+    verwaltung: { FM: 30000, OM: 150000, BL: 500000 },
+  },
+  {
+    payrollMonth: '2023-05',
+    stores: ['WIN', 'RAD', 'PHX'],
+    verwaltung: { FM: 20000 },
+  },
+  {
+    payrollMonth: '2023-06',
+    stores: ['WIN', 'RAD', 'PHX'],
+    verwaltung: { FM: 20000 },
+  },
+  {
+    payrollMonth: '2023-07',
+    stores: ['DIB', 'RAD', 'WIN', 'PHX'],
+    verwaltung: { FM: 30000, OM: 150000, BL: 500000 },
+  },
 ]
