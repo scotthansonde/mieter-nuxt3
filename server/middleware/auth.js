f265c16ffcc18a26cee887c963e94465dd36f896
Scott Hanson <papascott@gmail.com>
2023-09-29 12:29:33 +0200

    Only force api to auth in production

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@ import { getServerSession } from '#auth'
 
 export default defineEventHandler(async (event) => {
   const path = getRequestURL(event).pathname
-  if (path.startsWith('/api/') && !path.startsWith('/api/auth')) {
+  if (process.env.NODE_ENV === 'production' && path.startsWith('/api/') && !path.startsWith('/api/auth')) {
     const session = await getServerSession(event)
     if (!session) {
       if (!session) {
