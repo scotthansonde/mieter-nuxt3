274b8569073b15d75a009b6cf73d8499fd0d902a
Scott Hanson <papascott@gmail.com>
2023-10-22 09:48:20 +0200

    Test endpoint for web-cron

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,12 @@ import { getServerSession } from '#auth'
 
 export default defineEventHandler(async (event) => {
   const path = getRequestURL(event).pathname
-  if (process.env.NODE_ENV === 'production' && path.startsWith('/api/') && !path.startsWith('/api/auth')) {
+  if (
+    process.env.NODE_ENV === 'production' &&
+    path.startsWith('/api/') &&
+    !path.startsWith('/api/auth') &&
+    !path.startsWith('/api/cron')
+  ) {
     const session = await getServerSession(event)
     if (!session) {
       if (!session) {
