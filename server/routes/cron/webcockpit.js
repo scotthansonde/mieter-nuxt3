14c29546a17109e364299ede2ee69b282d449b70
Scott Hanson <papascott@gmail.com>
2023-10-26 16:41:01 +0200

    Cron endpoint to update webcockpit

---
+++ server/routes/cron/webcockpit.js
@@ -0,0 +1,38 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import { getWebcockpit } from '~~/server/utils/webcockpit'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+const storage = useStorage('data')
+
+dayjs.tz.setDefault('Europe/Berlin')
+const now = dayjs()
+const sevenAMBerlin = now.startOf('day').add(7, 'hour')
+const yesterday = now.subtract(1, 'day')
+const firstOfMonth = yesterday.date(1)
+
+const getLastTransactionDate = (cachedWebcockpit) => {
+  const { daily } = cachedWebcockpit.item
+  const dates = new Set()
+  daily.forEach((element) => {
+    const { entries } = element
+    dates.add(entries[entries.length - 1].transactionDate)
+  })
+  const date = Array.from(dates).sort().slice(-1)[0]
+  return date?.split('T')[0]
+}
+
+export default defineEventHandler(async (event) => {
+  const cachedWebcockpit = await storage.getItem('webcockpit')
+  const lastTransactionDate = getLastTransactionDate(cachedWebcockpit)
+  if (now.isAfter(sevenAMBerlin) && lastTransactionDate !== yesterday.format('YYYY-MM-DD')) {
+    await storage.setItem('webcockpit', '')
+    const startDate = firstOfMonth.format('YYYY-MM-DD')
+    const endDate = yesterday.format('YYYY-MM-DD')
+    const data = await getWebcockpit(startDate, endDate)
+    await storage.setItem('webcockpit', data)
+  }
+  return 'OK'
+})
